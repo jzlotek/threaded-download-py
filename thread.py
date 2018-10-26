4e93e478cf61ba4e289efea741db9808c6f86e79
John Zlotek <jzlotek@gmail.com>
2018-10-26 16:28:31 -0400

    Update thread.py

--- thread.py
+++ thread.py
@@ -1,15 +1,16 @@
+#!/usr/bin/env python
 import threading
 import sys
 import requests
 
-url = "http://media.mnn.com/assets/images/2018/07/cat_eating_fancy_ice_cream.jpg.838x0_q80.jpg"
+# url = "http://media.mnn.com/assets/images/2018/07/cat_eating_fancy_ice_cream.jpg.838x0_q80.jpg"
 
 
 def split_url_list(urls, num):
     ret = [[] for x in range(num)]
 
     for x in range(len(urls)):
-        ret[x % num].append(url)
+        ret[x % num].append(urls[x])
 
     return filter(lambda l: len(l) > 0, ret)
 
@@ -19,20 +20,20 @@ def runner(progress, files, printing=False):
         progress[3] = len(files)
 
     for f_name in files:
+        r = requests.get(f_name, stream=True)
+        local_filename = f_name.split('/')[-1]
         if printing:
             progress[2] += 1
             progress[1] = local_filename
             size_max = r.headers['Content-Length']
             size_dl = 0
-            :wq
-        r = requests.get(f_name, stream=True)
-        local_filename = f_name.split('/')[-1]
         with open(local_filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024):
                 if chunk:
-                    size_dl += len(chunk)
+                    if printing:
+                        size_dl += len(chunk)
+                        progress[0] = float(size_dl) / float(size_max) * 100
                     f.write(chunk)
-                    progress[0] = float(size_dl) / float(size_max) * 100
 
     return
 
@@ -66,6 +67,7 @@ def progress_print(p):
 if __name__ == "__main__":
     threads = []
     num = 1
+    printing = False
 
     if '-t' in sys.argv:
         try:
@@ -75,7 +77,11 @@ if __name__ == "__main__":
             print("Error trying to parse thread count | option: -t")
             exit(1)
 
-    files = [url for x in range(num * 5)]
+    files = []
+
+    for line in sys.stdin:
+        files.append(line.strip())
+
     files = split_url_list(files, num)
 
     # make sure we don't spawn additional threads
@@ -84,13 +90,16 @@ if __name__ == "__main__":
 
     progress = [[0, None, 0, 0] for x in range(num)]
 
+    if '-v' in sys.argv:
+        printing = True
+        threads.append(threading.Thread(target=progress_print,
+                                        args=(progress,)))
+                                        
     for x in range(num):
         threads.append(threading.Thread(target=runner,
                                         args=(progress[x],
-                                              files[x])))
-    if '-v' in sys.argv:
-        threads.append(threading.Thread(target=progress_print,
-                                        args=(progress,)))
+                                              files[x],
+                                              printing)))
 
     for thread in threads:
         thread.start()
