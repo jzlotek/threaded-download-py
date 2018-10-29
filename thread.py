149fae8eb33caa57b555a665d45ae14e42ed23dd
John Zlotek <jzlotek@gmail.com>
2018-10-29 15:38:58 -0400

    Add command line args

--- thread.py
+++ thread.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
+"""
+Usage is for large simutaneous file download
+It seems as if small downloads have minimal affect
+in terms of speed up across multiple threads due
+to the overhead of setting the threads up.
+
+
+-v Verbose option also slows down script execution for smaller files
+
+
+"""
+
 import threading
 import sys
 import requests
-
-# url = "http://media.mnn.com/assets/images/2018/07/cat_eating_fancy_ice_cream.jpg.838x0_q80.jpg"
+import time
+import os
 
 
 def split_url_list(urls, num):
@@ -15,7 +27,7 @@ def split_url_list(urls, num):
     return filter(lambda l: len(l) > 0, ret)
 
 
-def runner(progress, files, printing=False):
+def runner(progress, files, directory, printing=False, no_clobber=True):
     if printing:
         progress[3] = len(files)
 
@@ -27,7 +39,23 @@ def runner(progress, files, printing=False):
             progress[1] = local_filename
             size_max = r.headers['Content-Length']
             size_dl = 0
-        with open(local_filename, 'wb') as f:
+
+        if no_clobber and os.path.isfile(directory + local_filename):
+            counter = 0
+            s = local_filename.split('.')
+            ext = s.pop(-1)
+            s.append(str(counter))
+
+            while os.path.isfile(directory + "_".join(s) + '.' + ext):
+                counter += 1
+                s[-1] = str(counter)
+
+            local_filename = "_".join(s) + '.' + ext
+            if printing:
+                progress[1] = local_filename
+
+
+        with open(directory + local_filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024):
                 if chunk:
                     if printing:
@@ -53,7 +81,7 @@ def progress_print(p):
         for x in p:
             up()
         for x in range(len(p)):
-            string += "[%-20s] %.2f | %s | (%d of %d)\n" % (
+            string += "[%-20s] %-6.2f%% < %s > (%d of %d)\n" % (
                                                 '=' * (int(p[x][0]/5)-1) + '>',
                                                 float(p[x][0]),
                                                 p[x][1] if p[x][1] else "",
@@ -68,14 +96,31 @@ if __name__ == "__main__":
     threads = []
     num = 1
     printing = False
+    directory = './'
+    no_clobber = True
 
     if '-t' in sys.argv:
         try:
-            print(sys.argv[sys.argv.index('-t') + 1])
             num = int(sys.argv[sys.argv.index('-t') + 1])
         except Exception as e:
             print("Error trying to parse thread count | option: -t")
             exit(1)
+
+    if '-d' in sys.argv:
+        try:
+            directory = str(sys.argv[sys.argv.index('-d') + 1])
+            if directory[0] != '~':
+                directory = './' + directory
+            directory += '/'
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+
+        except Exception as e:
+            print("Error trying to parse directory | option: -d")
+            exit(1)
+
+    if '--clobber' in sys.argv:
+        no_clobber = False
 
     files = []
 
@@ -90,19 +135,33 @@ if __name__ == "__main__":
 
     progress = [[0, None, 0, 0] for x in range(num)]
 
+    printing_thread = None
     if '-v' in sys.argv:
         printing = True
-        threads.append(threading.Thread(target=progress_print,
-                                        args=(progress,)))
-                                        
+        printing_thread = threading.Thread(target=progress_print,
+                                        args=(progress,))
+
     for x in range(num):
         threads.append(threading.Thread(target=runner,
                                         args=(progress[x],
                                               files[x],
-                                              printing)))
+                                              directory,
+                                              printing,
+                                              no_clobber)))
+    timer = time.time()
+
+    if printing_thread is not None:
+        printing_thread.start()
 
     for thread in threads:
         thread.start()
 
     for thread in threads:
         thread.join()
+
+    if printing_thread is not None:
+        printing_thread.join()
+
+    timer = time.time() - timer
+    if printing:
+        print("Elapsed time: %-10.6fs" % timer)
